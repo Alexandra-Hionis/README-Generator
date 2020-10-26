3de3f10a663909d6c657abe9975b3a63b91172f7
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-10-26 16:58:24 -0400

    Created function generateMarkdwon answers and set up ReadMe format. Added async function init

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,4 +5,36 @@
 // `;
 // }
 
-// module.exports = generateMarkdown;
+function generateMarkdown(answers) {
+  return `
+<h1 align="center">${answers.projectTitle}</h1>
+## Description
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
+ ${answers.description}
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+${answers.installation}
+## Usage
+${answers.usage}
+## License
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+<br />
+This application is covered by the ${answers.license} license. 
+## Contributing
+${answers.contributing}
+## Tests
+${answers.tests}
+## Questions
+${answers.questions}<br />
+<br />
+Email me with any questions: ${answers.email}<br /><br />
+`;
+}
+module.exports = generateMarkdown;
